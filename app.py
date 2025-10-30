13449a337a1faee8705efe2e7f0028e4baef1535
Srinithis22 <srinithipriya5156@gmail.com>
2025-10-30 20:36:32 +0530

    Update greeting message with current time

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from the DevOps Project! The current time is: '
+    return 'Hello from the DevOps Project! The current time is: 8:36 '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
